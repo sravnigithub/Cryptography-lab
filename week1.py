b353d925ca4cb00417e30b9ca3b3756b4d52ca6e
sravnigithub <124333710+sravnigithub@users.noreply.github.com>
2024-04-08 19:30:36 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,4 +1,11 @@
-#week 1-1
+#week 1-1 Cprogram that contain sastring (charpointer)with the
+#value
+#‘Hello
+#world.’
+#TheprogramshouldXOReachcharacterinthisstringwith0and
+#displays the result.
+
+
 def xor_with_zero(input_string):
     result = ''
     for char in input_string:
@@ -9,7 +16,10 @@ input_string = 'Hello world.'
 result = xor_with_zero(input_string)
 print("Result after XORing each character with 0:", result)
 
-#week 1-2
+#week 1-2  
+#Cprogram that contain sastring(charpointer)with avalue‘ Hello
+#world’.The program should ANDorandXO Reach character in this
+#string with 127 and display the result.
 def and_xor_with_127(input_string):
     and_result = ''
     xor_result = ''
